7b919aee5bbfd9936ed32721323a68796665f306
liangjing <liangj@tangdou.com>
2018-09-27 20:22:19 +0800

    完善登录功能,登录页样式优化

--- test_platform/user_app/views.py
+++ test_platform/user_app/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.contrib import auth
+from django.contrib.auth.decorators import login_required
 
 
-# Create your views here.
 # 主要代码逻辑
 def index(request):
     return render(request, "index.html")
@@ -9,12 +11,26 @@ def index(request):
 
 # 处理登录请求
 def login_action(request):
-    if request.method == "GET":
-        username = request.GET.get("username")
-        password = request.GET.get("password")
+    if request.method == "POST":
+        username = request.POST.get("username", '')
+        password = request.POST.get("password", '')
+        user = auth.authenticate(username=username, password=password)
 
         if username == "" or password == "":
             return render(request, "index.html",
                           {"error": "用户名或者密码为空"})
+        elif user is not None:
+            auth.login(request, user)  # 登录验证
+            request.session['user'] = username  # 将session信息记录到浏览器
+            return HttpResponseRedirect('/project_manage/')
+        else:
+            return render(request, "index.html",
+                          {"error": "用户名或密码错误"})
+
+
+@login_required
+def project_manage(request):
+    return render(request, "project_manage.html")
+
 
 
