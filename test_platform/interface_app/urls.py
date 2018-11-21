4f1d7b650525fc81765aaade4aa82a9ad1311fcf
liangjing <liangj@tangdou.com>
2018-11-21 12:03:31 +0800

    更新项目管理页

--- test_platform/interface_app/urls.py
+++ test_platform/interface_app/urls.py
@@ -1,19 +1,21 @@
 from django.urls import path
-from interface_app.views import testcase_views
+from interface_app.views import testcase_views, testcase_api
 
 
 urlpatterns = [
     # case_manage path
     # ex : /interface/case_manage
-    # 接口用例管理
+    # 用例管理
     path('case_manage/', testcase_views.case_manage),
     path('add_case/', testcase_views.add_case),
-    path('api_debug/', testcase_views.api_debug),
-    path('save_case/', testcase_views.save_case),
-    path('get_project_list/', testcase_views.get_project_list),
     path('search_case_name/', testcase_views.search_case_name),
-
     path("debug_case/<int:cid>/", testcase_views.debug_case),
+
+    # 用例管理  --> 由JS调用的接口
+    path('get_project_list/', testcase_views.get_project_list),
+    path('api_debug/', testcase_views.api_debug),
+    path("api_assert/", testcase_api.api_assert),   # 待完善
+    path('save_case/', testcase_views.save_case),
     path("get_case_info/", testcase_views.get_project_list),
 ]
 
---
+++ test_platform/interface_app/views/testcase_api.py
@@ -0,0 +1,11 @@
+import json
+import requests
+from test_platform import common
+from interface_app.models import TestCase
+from project_app.models import Project, Module
+
+"""
+说明：该文件中的接口由前端JS调用，返回JSON格式数据
+"""
+
+
